6bca84960d63d3f4ac231e6a9365d2210e29fcea
khaled Mohamed Abdelghany <khaled.m.abdelghany@gmail.com>
2021-07-13 18:36:30 +0200

    Adding some SELECT/DELETE queries to Repository

--- src/main/java/com/spring/SpringdataJPA/StudentRepository.java
+++ src/main/java/com/spring/SpringdataJPA/StudentRepository.java
@@ -19,8 +19,29 @@ public interface StudentRepository extends JpaRepository<Student,Long> {
     @Query("SELECT s FROM Student s WHERE s.email =?1")
     Student findStudentByEmail(String email);
 
+    @Transactional
     @Modifying()
     @Query(value = "update Student s SET email =:newEmail WHERE s.email =:oldEmail",nativeQuery = true)
-    @Transactional
     void updateStudentEmail(@Param("oldEmail")String oldEmail ,@Param("newEmail") String newEmail);
+
+
+    /*
+    next is different select/Delete queries
+        @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.age >= ?2")
+    List<Student> selectStudentWhereFirstNameAndAgeGreaterOrEqual(
+            String firstName, Integer age);
+
+    @Query(
+            value = "SELECT * FROM student WHERE first_name = :firstName AND age >= :age",
+            nativeQuery = true)
+    List<Student> selectStudentWhereFirstNameAndAgeGreaterOrEqualNative(
+            @Param("firstName") String firstName,
+            @Param("age") Integer age);
+
+                @Transactional
+    @Modifying
+    @Query("DELETE FROM Student u WHERE u.id = ?1")
+    int deleteStudentById(Long id);
+
+     */
 }
